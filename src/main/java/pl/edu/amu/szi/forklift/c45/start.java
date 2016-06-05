9a0ac62cead419a2ba9f9035d63ddb30341e7600
Łukasz Prażyński <praza99+git@gmail.com>
2016-06-06 01:00:52 +0200

    Base Tree + examples
    There are also implemented method's for calling it from our app check -
    generateFunClassification

---
+++ src/main/java/pl/edu/amu/szi/forklift/c45/start.java
@@ -0,0 +1,20 @@
+package pl.edu.amu.szi.forklift.c45;
+
+public class start {
+	public static void main(String[] args) {
+
+
+//		Tree tree = new Tree();
+//		tree.generateBestTree(dataFile, 1000);
+//		tree.generateClassification(testFile);
+
+		Tree tree = new Tree();
+
+
+			String dataFile = "F:\\Github\\...\\C\\trainProdSelection.arff";
+			String testFile = "F:\\Github\\...\\C\\testProdSelection.arff";
+			tree.generateBestTree(dataFile, 1000);
+			tree.generateClassification(testFile);
+
+	}
+}
